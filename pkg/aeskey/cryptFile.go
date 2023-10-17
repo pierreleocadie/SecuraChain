a396be26bbc00c43f6a9720758a441c118a98a38
Jordan10980 <jordandohou11@gmail.com>
2023-10-17 23:19:11 +0200

    feat(aeskey): Implement AES encryption and decryption Implemented functions to encrypt and decrypt files using AES encryption. Added support to generate and manage 32-byte keys for AES256 encryption and decryption. Conducted thorough testing for AES key generation, encryption, and decryption.

---
+++ pkg/aeskey/cryptFile.go
@@ -0,0 +1,97 @@
+package aeskey
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+// EncryptFile encrypts a file using AES encryption.
+func EncryptFile(inputFilePath, outputFilePath string, key []byte) error {
+	// Read file
+	inputFile, err := os.Open(filepath.Clean(inputFilePath))
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(outputFilePath)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	// Create an AES cipher block using the provided key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	// Generate a random initialization vector (IV)
+	initializationVector := make([]byte, aes.BlockSize)
+	if _, err := rand.Read(initializationVector); err != nil {
+		return err
+	}
+
+	// Write the IV to the output file (it will be needed for decryption)
+	if _, err := outputFile.Write(initializationVector); err != nil {
+		return err
+	}
+
+	// Create a cipher block mode for AES in CFB mode
+	stream := cipher.NewCFBEncrypter(block, initializationVector)
+
+	// Create a writer with the encryption stream
+	writer := &cipher.StreamWriter{S: stream, W: outputFile}
+
+	// Copy and encrypt the file content
+	if _, err := io.Copy(writer, inputFile); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DecryptFile decrypts a file that was encrypted using AES encryption.
+func DecryptFile(inputFilePath, outputFilePath string, key []byte) error {
+	// Open the input and output files
+	inputFile, err := os.Open(filepath.Clean(inputFilePath))
+	if err != nil {
+		return err
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(outputFilePath)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	// Create an AES cipher block using the provided key
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return err
+	}
+
+	// Read the initialization vector (IV) from the input file
+	initializationVector := make([]byte, aes.BlockSize)
+	if _, err := inputFile.Read(initializationVector); err != nil {
+		return err
+	}
+
+	// Create a cipher block mode for AES in CFB mode
+	stream := cipher.NewCFBDecrypter(block, initializationVector)
+
+	// Create a reader with the decryption stream
+	reader := &cipher.StreamReader{S: stream, R: inputFile}
+
+	// Copy and decrypt the file content
+	if _, err := io.Copy(outputFile, reader); err != nil {
+		return err
+	}
+
+	return nil
+}
