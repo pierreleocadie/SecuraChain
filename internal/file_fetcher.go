f93b76dad3519e36054fbb708149df7a03812020
Jordan10980 <jordandohou11@gmail.com>
2023-12-03 13:55:41 +0100

    Removing the function FetchFileFromIPFSNetwork in file_fetcher.go

--- internal/file_fetcher.go
+++ internal/file_fetcher.go
@@ -36,31 +36,3 @@ func FetchFileFromIPFS(ctx context.Context, ipfsApi icore.CoreAPI, cidFile path.
 
 	return nil
 }
-
-func FetchFileFromIPFSNetwork(ctx context.Context, ipfsApi icore.CoreAPI, peerCidFile path.ImmutablePath) error {
-	outputBasePathNetwork := "./Ipfs-files-downloaded/Network/"
-
-	// S'assurez que le dossier de sortie existe
-	if err := os.MkdirAll(outputBasePathNetwork, 0755); err != nil {
-		return fmt.Errorf("error creating output directory: %v", err)
-	}
-	exampleCIDStr := peerCidFile.RootCid().String()
-
-	fmt.Printf("Fetching a file from the network with CID %s\n", exampleCIDStr)
-	outputPath := outputBasePathNetwork + exampleCIDStr
-	testCID := path.FromCid(peerCidFile.RootCid())
-
-	rootNode, err := ipfsApi.Unixfs().Get(ctx, testCID)
-	if err != nil {
-		return fmt.Errorf("could not get file with CID: %s", err)
-	}
-
-	err = files.WriteTo(rootNode, outputPath)
-	if err != nil {
-		panic(fmt.Errorf("could not write out the fetched CID: %s", err))
-	}
-
-	fmt.Printf("Wrote the file to %s\n", outputPath)
-
-	return nil
-}
