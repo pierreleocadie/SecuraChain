d4388a1893c55edbf2ab86ae02d5241a137f83d0
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2023-12-20 00:23:15 +0100

    feat(client): Initiate Fyne tests for user client GUI
    This commit marks the beginning of our tests with the Fyne library for the graphical user interface (GUI) of the user client node. The changes are made on the dedicated client branch.
    
    These initial tests lay the groundwork for the development of the user client GUI using Fyne.
    
    BREAKING CHANGE: Initiate Fyne tests for user client GUI

--- cmd/securachain-user-client/main.go
+++ cmd/securachain-user-client/main.go
@@ -2,8 +2,164 @@ package main
 
 import (
 	"log"
+	"net/url"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+	"github.com/pierreleocadie/SecuraChain/pkg/aes"
+	"github.com/pierreleocadie/SecuraChain/pkg/ecdsa"
 )
 
 func main() {
-	log.Println("Hello, World!")
+	a := app.New()
+	w := a.NewWindow("SecuraChain User Client")
+	w.Resize(fyne.NewSize(800, 600))
+
+	// Create an input to set file path with a browse button to save the ECDSA key pair
+	ecdsaInput := widget.NewLabel("")
+	ecdsaBrowseButton := widget.NewButton("Browse", func() {
+		dialog := dialog.NewFolderOpen(func(dir fyne.ListableURI, err error) {
+			if err == nil && (dir != nil) {
+				u, er := url.Parse(dir.String())
+				if er == nil {
+					ecdsaInput.SetText(u.Path)
+				}
+			}
+		}, w)
+		dialog.Show()
+	})
+
+	// Create an input to set file path with a browse button to save the AES key
+	aesInput := widget.NewLabel("")
+	aesBrowseButton := widget.NewButton("Browse", func() {
+		dialog := dialog.NewFolderOpen(func(dir fyne.ListableURI, err error) {
+			if err == nil && (dir != nil) {
+				u, er := url.Parse(dir.String())
+				if er == nil {
+					aesInput.SetText(u.Path)
+				}
+			}
+		}, w)
+		dialog.Show()
+	})
+
+	// Create a button to load an ECDSA key pair
+	ecdsaButtonLoad := widget.NewButton("Load ECDSA Key Pair", func() {
+		ecdsaKeyPair, err := ecdsa.LoadKeys("ecdsaPrivateKey", "ecdsaPublicKey", ecdsaInput.Text)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(ecdsaKeyPair)
+	})
+
+	// Create a button to generate a new ECDSA key pair
+	ecdsaButton := widget.NewButton("Generate ECDSA Key Pair", func() {
+		ecdsaKeyPair, err := ecdsa.NewECDSAKeyPair()
+		if err != nil {
+			return
+		}
+		err = ecdsaKeyPair.SaveKeys("ecdsaPrivateKey", "ecdsaPublicKey", ecdsaInput.Text)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	})
+
+	// Create a button to load an AES key
+	var aesKey aes.Key
+	aesButtonLoad := widget.NewButton("Load AES Key", func() {
+		aesKeyL, err := aes.LoadKey("aesKey", aesInput.Text)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		aesKey = aesKeyL
+		log.Println(aesKey)
+	})
+
+	// Create a button to generate a new AES key
+	aesButton := widget.NewButton("Generate AES Key", func() {
+		aesKey, err := aes.NewAESKey()
+		if err != nil {
+			return
+		}
+		err = aesKey.SaveKey("aesKey", aesInput.Text)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	})
+
+	// Create a new button to select a file
+	selectedFileLabel := widget.NewLabel("")
+	selectFileButton := widget.NewButton("Select File", func() {
+		dialog := dialog.NewFileOpen(func(file fyne.URIReadCloser, err error) {
+			if err == nil && (file != nil) {
+				selectedFileLabel.SetText(file.URI().Path())
+			}
+		}, w)
+		dialog.Show()
+	})
+
+	// create a new button to send a file over the network
+	/* sendFileButton := widget.NewButton("Send File", func() {
+		// TODO
+		// 1. Encrypt the file with AES
+		if aesKey == nil {
+			log.Println("AES key not loaded")
+			return
+		}
+
+		encryptedFilePath := selectedFileLabel.Text + ".enc"
+		err := aesKey.EncryptFile(selectedFileLabel.Text, encryptedFilePath)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// 2. Compute the checksum of the encrypted file
+		// 3. Get the size of the encrypted file
+		// 4. Get the name of the encrypted file
+		// 5. Get the extension of the encrypted file
+		// 6. Create a new ClientAnnouncement
+	}) */
+
+	hBoxECDSA := container.New(
+		layout.NewHBoxLayout(),
+		ecdsaInput,
+		layout.NewSpacer(),
+		ecdsaBrowseButton,
+	)
+	hBoxAES := container.New(
+		layout.NewHBoxLayout(),
+		aesInput,
+		layout.NewSpacer(),
+		aesBrowseButton,
+	)
+	hBoxSelectFile := container.New(
+		layout.NewHBoxLayout(),
+		selectFileButton,
+		selectedFileLabel,
+	)
+	vBox := container.New(
+		layout.NewVBoxLayout(),
+		widget.NewLabel("ECDSA Key Pair:"),
+		hBoxECDSA,
+		ecdsaButtonLoad,
+		ecdsaButton,
+		widget.NewLabel("AES Key:"),
+		hBoxAES,
+		aesButtonLoad,
+		aesButton,
+		widget.NewLabel("Select File:"),
+		hBoxSelectFile,
+	)
+
+	w.SetContent(vBox)
+	w.ShowAndRun()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/pierreleocadie/SecuraChain
 go 1.21.3
 
 require (
+	fyne.io/fyne/v2 v2.4.2
 	github.com/google/uuid v1.3.1
 	github.com/ipfs/boxo v0.10.0
 	github.com/ipfs/go-cid v0.4.1
@@ -12,20 +13,31 @@ require (
 )
 
 require (
+	fyne.io/systray v1.10.1-0.20231115130155-104f5ef7839e // indirect
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/containerd/cgroups v1.1.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/davidlazar/go-crypto v0.0.0-20200604182044-b73af7476f6c // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/elastic/gosigar v0.14.2 // indirect
 	github.com/flynn/noise v1.0.0 // indirect
 	github.com/francoispqt/gojay v1.2.13 // indirect
+	github.com/fredbi/uri v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/fyne-io/gl-js v0.0.0-20220119005834-d2da28d9ccfe // indirect
+	github.com/fyne-io/glfw-js v0.0.0-20220120001248-ee7290d23504 // indirect
+	github.com/fyne-io/image v0.0.0-20220602074514-4956b0afb3d2 // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/go-text/render v0.0.0-20230619120952-35bccb6164b8 // indirect
+	github.com/go-text/typesetting v0.0.0-20230616162802-9c17dd34aa4a // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
@@ -51,6 +63,7 @@ require (
 	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
 	github.com/jbenet/go-temp-err-catcher v0.1.0 // indirect
 	github.com/jbenet/goprocess v0.1.4 // indirect
+	github.com/jsummers/gobmp v0.0.0-20151104160322-e2ba15ffa76e // indirect
 	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/koron/go-ssdp v0.0.4 // indirect
@@ -89,6 +102,7 @@ require (
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/polydawn/refmt v0.89.0 // indirect
 	github.com/prometheus/client_golang v1.16.0 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
@@ -100,7 +114,12 @@ require (
 	github.com/quic-go/webtransport-go v0.6.0 // indirect
 	github.com/raulk/go-watchdog v1.3.0 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
+	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/tevino/abool v1.2.0 // indirect
 	github.com/whyrusleeping/go-keyspace v0.0.0-20160322163242-5b898ac5add1 // indirect
+	github.com/yuin/goldmark v1.5.5 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/otel v1.16.0 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
@@ -113,6 +132,8 @@ require (
 	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/image v0.11.0 // indirect
+	golang.org/x/mobile v0.0.0-20230531173138-3c911d8e3eda // indirect
 	golang.org/x/mod v0.13.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sync v0.4.0 // indirect
@@ -121,5 +142,7 @@ require (
 	golang.org/x/tools v0.14.0 // indirect
 	gonum.org/v1/gonum v0.13.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/js/dom v0.0.0-20210725211120-f030747120f2 // indirect
 	lukechampine.com/blake3 v1.2.1 // indirect
 )
