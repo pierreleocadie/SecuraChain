fbf0620dc0106facf787895ad46269480d10ac76
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2023-12-27 15:51:06 +0100

    refactor: constants, node initialization and DHT discovery

---
+++ internal/config/constants.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	ListeningPortFlag             = 0
+	LowWater                      = 160
+	HighWater                     = 192
+	UserAgent                     = "SecuraChain"
+	ProtocolVersion               = "0.0.1"
+	RendezvousStringFlag          = "SecuraChainNetwork"
+	ClientAnnouncementStringFlag  = "ClientAnnouncement"
+	StorageNodeResponseStringFlag = "StorageNodeResponse"
+	DHTDiscoveryRefreshInterval   = 10 * time.Second
+)
+
+var (
+	Ip4tcp         = fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", ListeningPortFlag)
+	Ip6tcp         = fmt.Sprintf("/ip6/::/tcp/%d", ListeningPortFlag)
+	Ip4quic        = fmt.Sprintf("/ip4/0.0.0.0/udp/%d/quic-v1", ListeningPortFlag)
+	Ip6quic        = fmt.Sprintf("/ip6/::/udp/%d/quic-v1", ListeningPortFlag)
+	BootstrapPeers = []string{
+		"/ip4/13.37.148.174/udp/1211/quic-v1/p2p/12D3KooWBm6aEtcGiJNsnsCwaiH4SoqJHZMgvctdQsyAenwyt8Ds",
+		"/ip4/154.56.63.167/udp/1211/quic-v1/p2p/12D3KooWBa1WvTGddVA8z8dtXDvJBiEYwZXj7HB2d9AMEFW41UJx",
+	}
+)
