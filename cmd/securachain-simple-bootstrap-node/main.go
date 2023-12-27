44343a156dc3ca423bc8c4d36784041121e7e342
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2023-12-27 15:53:58 +0100

    refactor(simple-bootstrap-node): use node package for node init and dht

--- cmd/securachain-simple-bootstrap-node/main.go
+++ cmd/securachain-simple-bootstrap-node/main.go
@@ -7,106 +7,11 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/event"
-	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
-	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
-	libp2pquic "github.com/libp2p/go-libp2p/p2p/transport/quic"
-	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
-	"github.com/multiformats/go-multiaddr"
-	"github.com/pierreleocadie/SecuraChain/internal/discovery"
+	"github.com/pierreleocadie/SecuraChain/internal/node"
 )
-
-const (
-	listeningPortFlag = 1211
-	lowWater          = 160
-	highWater         = 192
-)
-
-var (
-	rendezvousStringFlag = fmt.Sprintln("SecuraChainNetwork")
-	ip4tcp               = fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", listeningPortFlag)
-	ip6tcp               = fmt.Sprintf("/ip6/::/tcp/%d", listeningPortFlag)
-	ip4quic              = fmt.Sprintf("/ip4/0.0.0.0/udp/%d/quic-v1", listeningPortFlag)
-	ip6quic              = fmt.Sprintf("/ip6/::/udp/%d/quic-v1", listeningPortFlag)
-)
-
-func initializeNode() host.Host {
-	/*
-	* NODE INITIALIZATION
-	 */
-	// Create a new connection manager - Exactly the same as the default connection manager but with a grace period
-	connManager, err := connmgr.NewConnManager(lowWater, highWater, connmgr.WithGracePeriod(time.Minute))
-	if err != nil {
-		panic(err)
-	}
-
-	// Create a new libp2p Host
-	host, err := libp2p.New(
-		libp2p.UserAgent("SecuraChain"),
-		libp2p.ProtocolVersion("0.0.1"),
-		libp2p.EnableNATService(),
-		libp2p.NATPortMap(),
-		libp2p.EnableHolePunching(),
-		libp2p.ListenAddrStrings(ip4tcp, ip6tcp, ip4quic, ip6quic),
-		libp2p.ConnectionManager(connManager),
-		libp2p.Transport(tcp.NewTCPTransport),
-		libp2p.Transport(libp2pquic.NewTransport),
-		libp2p.RandomIdentity,
-		libp2p.DefaultSecurity,
-		libp2p.DefaultMuxers,
-		libp2p.DefaultEnableRelay,
-		libp2p.EnableRelayService(),
-	)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Our node ID: %s\n", host.ID())
-
-	// Node info
-	hostInfo := peer.AddrInfo{
-		ID:    host.ID(),
-		Addrs: host.Addrs(),
-	}
-
-	addrs, err := peer.AddrInfoToP2pAddrs(&hostInfo)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, addr := range addrs {
-		log.Println("Node address: ", addr)
-	}
-
-	for _, addr := range host.Addrs() {
-		log.Println("Listening on address: ", addr)
-	}
-
-	return host
-}
-
-func setupDHTDiscovery(ctx context.Context, host host.Host) {
-	/*
-	* NETWORK PEER DISCOVERY WITH DHT
-	 */
-	// Initialize DHT in server mode
-	dhtDiscovery := discovery.NewDHTDiscovery(
-		true,
-		rendezvousStringFlag,
-		[]multiaddr.Multiaddr{},
-		0,
-	)
-
-	// Run DHT
-	if err := dhtDiscovery.Run(ctx, host); err != nil {
-		log.Println("Failed to run DHT: ", err)
-		return
-	}
-}
 
 func waitForTermSignal() {
 	// wait for a SIGINT or SIGTERM signal
@@ -120,12 +25,15 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	/*
+	* NODE LIBP2P
+	 */
 	// Initialize the node
-	host := initializeNode()
+	host := node.Initialize()
 	defer host.Close()
 
 	// Setup DHT discovery
-	setupDHTDiscovery(ctx, host)
+	node.SetupDHTDiscovery(ctx, host, false)
 
 	/*
 	* DISPLAY PEER CONNECTEDNESS CHANGES
