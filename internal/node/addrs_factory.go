bb9c697bbd7b44d8181bcf7eda3b7b061dfa4017
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2023-12-30 01:15:22 +0100

    refactor: isPrivateAddr function to handle IPv6 and local addresses as well
    This commit refactors the `isPrivateAddr` function in `addrs_factory.go` to handle both IPv4 and IPv6 addresses. Previously, if an IPv4 address was not found, the function would return false. Now, it attempts to retrieve the IPv6 address and handles any errors accordingly. Additionally, the function now checks if the IP address is a loopback address in addition to checking if it is in a private range. This ensures more accurate filtering of private network addresses.

--- internal/node/addrs_factory.go
+++ internal/node/addrs_factory.go
@@ -11,8 +11,12 @@ func isPrivateAddr(maddr ma.Multiaddr) bool {
 	// Extract the IP address from the multiaddress
 	ip, err := maddr.ValueForProtocol(ma.P_IP4)
 	if err != nil {
-		// Handle the error or ignore the address
-		return false
+		// Attempt to get the IPv6 address if IPv4 was not found
+		ip, err = maddr.ValueForProtocol(ma.P_IP6)
+		if err != nil {
+			// Handle the error or ignore the address
+			return false
+		}
 	}
 
 	// Parse the IP address
@@ -21,8 +25,8 @@ func isPrivateAddr(maddr ma.Multiaddr) bool {
 		return false
 	}
 
-	// Check if the IP is in a private range
-	return netIP.IsPrivate()
+	// Check if the IP is in a private range or is a loopback address
+	return netIP.IsPrivate() || netIP.IsLoopback()
 }
 
 // filterOutPrivateAddrs filters out private network addresses
