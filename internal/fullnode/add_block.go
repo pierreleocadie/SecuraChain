af7db571fd1aac95530142136c17e3131d97b576
Jordan10980 <jordandohou11@gmail.com>
2024-02-06 10:23:06 +0100

    style(add_block.go): code clarification according to the development charter

--- internal/fullnode/add_block.go
+++ internal/fullnode/add_block.go
@@ -1,22 +1,36 @@
 package fullnode
 
 import (
+	"fmt"
+
 	"github.com/pierreleocadie/SecuraChain/internal/core/block"
 	"github.com/pierreleocadie/SecuraChain/internal/fullnode/pebble"
 )
 
+// AddBlockToBlockchain adds a block to the blockchain if it is not already present.
+// It returns a boolean indicating whether the block was added, and a message.
 func AddBlockToBlockchain(database *pebble.PebbleTransactionDB, blockAnnounced *block.Block) (bool, string) {
+	// Use the block's signature as a unique key for storage.
 	key := blockAnnounced.Signature
+
+	// Attempt to retrieve the block by its signature to checj for its existence in the database.
 	existingBlock, err := database.GetBlock(key)
-	if err == nil && existingBlock != nil {
-		return false, "Block already existing in the blockchain"
+	if err != nil {
+		// An error here means there was a problem accessing the database, not that the block doesn't exist.
+		return false, fmt.Sprintf("Error checking for the block existence in the database: %s", err)
 	}
 
-	//if the block is not in the blockchain, add it
+	if existingBlock != nil {
+		// The bloc already exists in the database, so there's no need to add it again.
+		return false, "Block already exists in the blockchain"
+	}
+
+	// If the block is not in the blockchain, add it.
 	err = database.SaveBlock(key, blockAnnounced)
 	if err != nil {
-		return false, "Error adding block to the database"
-	} else {
-		return true, "Block addded succesfully to the blockchain "
+		// An error occurred while trying to save the block to the database.
+		return false, fmt.Sprintf("Error adding block to the database: %s", err)
 	}
+
+	return true, "Block addded succesfully to the blockchain "
 }
