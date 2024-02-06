77d12aa1020486260a3d4204465694f4e244223f
Jordan10980 <jordandohou11@gmail.com>
2024-02-06 10:35:52 +0100

    style(storageBlock.go): code clarification according to the devlopment charter

--- internal/fullnode/pebble/storageBlock.go
+++ internal/fullnode/pebble/storageBlock.go
@@ -8,59 +8,59 @@ import (
 	"github.com/pierreleocadie/SecuraChain/internal/core/block"
 )
 
-// TransactionDatabase est une interface pour interagir avec la base de données des transactions.
+// TransactionDatabase defines an interface for interacing with the transaction database.
 type TransactionDatabase interface {
-	SaveTransaction(key []byte, bx block.Block) error
-	GetTransaction(key []byte) (block.Block, error)
+	SaveBlock(key []byte, bx block.Block) error
+	GetBlock(key []byte) (block.Block, error)
 	Close() error
 }
 
-// PebbleTransactionDB est une implémentation de l'interface TransactionDatabase utilisant la bibliothèque Pebble.
+// PebbleTransactionDB implements the TransactionDatabase interface using the Pebble library.
 type PebbleTransactionDB struct {
 	db *pebble.DB
 }
 
-// NewPebbleTransactionDB crée une nouvelle instance de PebbleTransactionDB.
+// NewPebbleTransactionDB creates a new instance of PebbleTransactionDB.
 func NewPebbleTransactionDB(dbPath string) (*PebbleTransactionDB, error) {
 	options := &pebble.Options{}
 	db, err := pebble.Open(dbPath, options)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open pebble database : %v", err)
 	}
 	return &PebbleTransactionDB{db: db}, nil
 }
 
-// SaveTransaction enregistre une transaction dans la base de données.
+// SaveBlock stores a block in the database.
 func (pdb *PebbleTransactionDB) SaveBlock(key []byte, bx *block.Block) error {
 	bxBytes, err := json.Marshal(bx)
 	if err != nil {
-		return fmt.Errorf("erreur de sérialisation de la transaction: %v", err)
+		return fmt.Errorf("error serializing block: %v", err)
 	}
 
 	err = pdb.db.Set(key, bxBytes, pebble.Sync)
 	if err != nil {
-		return fmt.Errorf("erreur d'enregistrement de la transaction: %v", err)
+		return fmt.Errorf("error saving block to the database : %v", err)
 	}
 	return nil
 }
 
-// GetTransaction récupère une transaction à partir de la base de données.
+// GetBlock retrives a block from the database.
 func (pdb *PebbleTransactionDB) GetBlock(key []byte) (*block.Block, error) {
 	bxBytes, closer, err := pdb.db.Get(key)
 	if err != nil {
-		return nil, fmt.Errorf("erreur de récupération de la transaction: %v", err)
+		return nil, fmt.Errorf("error retrieving block from the database: %v", err)
 	}
 	defer closer.Close()
 
-	blockDeserialize, err := block.DeserializeBlock(bxBytes)
+	var blockDeserialize *block.Block
+	err = json.Unmarshal(bxBytes, &blockDeserialize)
 	if err != nil {
-		return nil, fmt.Errorf("erreur de désérialisation de la transaction: %v", err)
+		return nil, fmt.Errorf("error deserializing block: %v", err)
 	}
-
 	return blockDeserialize, nil
 }
 
-// Close ferme la connexion à la base de données.
+// Close closes the database connection.
 func (pdb *PebbleTransactionDB) Close() error {
 	return pdb.db.Close()
 }
