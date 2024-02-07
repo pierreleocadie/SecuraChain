8562605437c32c94aa9240d2cdb166a4f35b90f2
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-02-07 01:24:09 +0100

    fix(bootstrap-node): pubsub keep alive relay connection

--- cmd/securachain-simple-bootstrap-node/main.go
+++ cmd/securachain-simple-bootstrap-node/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"flag"
+	"time"
 
 	ipfsLog "github.com/ipfs/go-log/v2"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/event"
 	"github.com/libp2p/go-libp2p/core/network"
 	"github.com/pierreleocadie/SecuraChain/internal/config"
+	netwrk "github.com/pierreleocadie/SecuraChain/internal/network"
 	"github.com/pierreleocadie/SecuraChain/internal/node"
 	"github.com/pierreleocadie/SecuraChain/pkg/utils"
 )
@@ -55,7 +57,45 @@ func main() {
 		panic(err)
 	}
 
-	node.PubsubKeepRelayConnectionAlive(ctx, ps, cfg, log, host)
+	// node.PubsubKeepRelayConnectionAlive(ctx, ps, cfg, log, host)
+
+	// KeepRelayConnectionAlive
+	keepRelayConnectionAliveTopic, err := ps.Join(cfg.KeepRelayConnectionAliveStringFlag)
+	if err != nil {
+		log.Warnf("Failed to join KeepRelayConnectionAlive topic: %s", err)
+	}
+
+	// Subscribe to KeepRelayConnectionAlive topic
+	subKeepRelayConnectionAlive, err := keepRelayConnectionAliveTopic.Subscribe()
+	if err != nil {
+		log.Warnf("Failed to subscribe to KeepRelayConnectionAlive topic: %s", err)
+	}
+
+	// Handle incoming KeepRelayConnectionAlive messages
+	go func() {
+		for {
+			msg, err := subKeepRelayConnectionAlive.Next(ctx)
+			if err != nil {
+				log.Errorf("Failed to get next message from KeepRelayConnectionAlive topic: %s", err)
+				continue
+			}
+			log.Debugf("Received KeepRelayConnectionAlive message from %s", msg.GetFrom().String())
+			log.Debugf("KeepRelayConnectionAlive: %s", string(msg.Data))
+		}
+	}()
+
+	// Handle outgoing KeepRelayConnectionAlive messages
+	go func() {
+		for {
+			time.Sleep(cfg.KeepRelayConnectionAliveInterval)
+			err := keepRelayConnectionAliveTopic.Publish(ctx, netwrk.GeneratePacket(host.ID()))
+			if err != nil {
+				log.Errorf("Failed to publish KeepRelayConnectionAlive message: %s", err)
+				continue
+			}
+			log.Debugf("KeepRelayConnectionAlive message sent successfully")
+		}
+	}()
 
 	/*
 	* DISPLAY PEER CONNECTEDNESS CHANGES
