266576aed1ceb99087483433089961b219d72809
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-02-12 11:25:40 +0100

    test

--- cmd/securachain-test-node/main.go
+++ cmd/securachain-test-node/main.go
@@ -6,16 +6,9 @@ import (
 	"time"
 
 	ipfsLog "github.com/ipfs/go-log/v2"
-	"github.com/libp2p/go-libp2p"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/event"
-	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
-	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/p2p/host/autorelay"
-	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
-	libp2pquic "github.com/libp2p/go-libp2p/p2p/transport/quic"
-	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
 	"github.com/pierreleocadie/SecuraChain/internal/config"
 	netwrk "github.com/pierreleocadie/SecuraChain/internal/network"
 	"github.com/pierreleocadie/SecuraChain/internal/node"
@@ -49,71 +42,12 @@ func main() { //nolint: funlen, gocyclo
 	/*
 	* NODE LIBP2P
 	 */
-	var h host.Host
-
-	// Create a new connection manager - Exactly the same as the default connection manager but with a grace period
-	connManager, err := connmgr.NewConnManager(cfg.LowWater, cfg.HighWater, connmgr.WithGracePeriod(time.Minute))
-	if err != nil {
-		log.Panicf("Failed to create new connection manager: %s", err)
-	}
-
-	hostReady := make(chan struct{})
-	hostGetter := func() host.Host {
-		<-hostReady // closed when we finish setting up the host
-		return h
-	}
-
-	// Create a new libp2p Host
-	h, err = libp2p.New(
-		libp2p.UserAgent(cfg.UserAgent),
-		libp2p.ProtocolVersion(cfg.ProtocolVersion),
-		libp2p.AddrsFactory(netwrk.FilterOutPrivateAddrs), // Comment this line to build bootstrap node
-		libp2p.EnableNATService(),
-		// libp2p.NATPortMap(),
-		// libp2p.EnableHolePunching(),
-		libp2p.ListenAddrStrings(cfg.IP4tcp, cfg.IP6tcp, cfg.IP4quic, cfg.IP6quic),
-		libp2p.ConnectionManager(connManager),
-		libp2p.Transport(tcp.NewTCPTransport),
-		libp2p.Transport(libp2pquic.NewTransport),
-		libp2p.RandomIdentity,
-		libp2p.DefaultSecurity,
-		libp2p.DefaultMuxers,
-		libp2p.DefaultEnableRelay,
-		libp2p.EnableAutoRelayWithPeerSource(
-			netwrk.NewPeerSource(log, hostGetter),
-			autorelay.WithBackoff(cfg.DiscoveryRefreshInterval),
-			autorelay.WithMinInterval(cfg.DiscoveryRefreshInterval),
-			autorelay.WithNumRelays(1),
-			autorelay.WithMinCandidates(1),
-		),
-	)
-	if err != nil {
-		log.Panicf("Failed to create new libp2p Host: %s", err)
-	}
-	log.Debugf("Our node ID: %s\n", h.ID())
-
-	// Close the hostReady channel to signal that the host is ready
-	close(hostReady)
-
-	// Node info
-	hostInfo := peer.AddrInfo{
-		ID:    h.ID(),
-		Addrs: h.Addrs(),
-	}
-
-	addrs, err := peer.AddrInfoToP2pAddrs(&hostInfo)
-	if err != nil {
-		log.Panicf("Failed to convert peer.AddrInfo to p2p.Addr: %s", err)
-	}
-
-	for _, addr := range addrs {
-		log.Debugln("Node address: ", addr)
-	}
-
-	for _, addr := range h.Addrs() {
-		log.Debugln("Listening on address: ", addr)
-	}
+	/*
+	* NODE LIBP2P
+	 */
+	h := node.Initialize(log, *cfg)
 	defer h.Close()
+	log.Debugf("Storage node initialized with PeerID: %s", h.ID().String())
 
 	/*
 	* PUBSUB
