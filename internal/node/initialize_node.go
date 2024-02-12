5fa5502146eba2a7763d28a0718f85d9b038f8ef
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-02-12 16:20:35 +0100

    test

--- internal/node/initialize_node.go
+++ internal/node/initialize_node.go
@@ -10,7 +10,9 @@ import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/host/autorelay"
 	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
+	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
 	libp2pquic "github.com/libp2p/go-libp2p/p2p/transport/quic"
 	"github.com/libp2p/go-libp2p/p2p/transport/tcp"
 	"github.com/pierreleocadie/SecuraChain/internal/config"
@@ -30,11 +32,11 @@ func Initialize(log *ipfsLog.ZapEventLogger, cfg config.Config) host.Host { //no
 		log.Panicf("Failed to create new connection manager: %s", err)
 	}
 
-	// hostReady := make(chan struct{})
-	// hostGetter := func() host.Host {
-	// 	<-hostReady // closed when we finish setting up the host
-	// 	return h
-	// }
+	hostReady := make(chan struct{})
+	hostGetter := func() host.Host {
+		<-hostReady // closed when we finish setting up the host
+		return h
+	}
 
 	// Create a new libp2p Host
 	h, err = libp2p.New(
@@ -53,13 +55,13 @@ func Initialize(log *ipfsLog.ZapEventLogger, cfg config.Config) host.Host { //no
 		libp2p.DefaultMuxers,
 		libp2p.DefaultEnableRelay,
 		libp2p.EnableRelayService(),
-		// libp2p.EnableAutoRelayWithPeerSource(
-		// 	network.NewPeerSource(log, hostGetter),
-		// 	autorelay.WithBackoff(cfg.DiscoveryRefreshInterval),
-		// 	autorelay.WithMinInterval(cfg.DiscoveryRefreshInterval),
-		// 	autorelay.WithNumRelays(1),
-		// 	autorelay.WithMinCandidates(1),
-		// ),
+		libp2p.EnableAutoRelayWithPeerSource(
+			network.NewPeerSource(log, hostGetter),
+			autorelay.WithBackoff(cfg.DiscoveryRefreshInterval),
+			autorelay.WithMinInterval(cfg.DiscoveryRefreshInterval),
+			autorelay.WithNumRelays(1),
+			autorelay.WithMinCandidates(1),
+		),
 	)
 	if err != nil {
 		log.Panicf("Failed to create new libp2p Host: %s", err)
@@ -67,7 +69,7 @@ func Initialize(log *ipfsLog.ZapEventLogger, cfg config.Config) host.Host { //no
 	log.Debugf("Our node ID: %s\n", h.ID())
 
 	// Close the hostReady channel to signal that the host is ready
-	// close(hostReady)
+	close(hostReady)
 
 	// Node info
 	hostInfo := peer.AddrInfo{
@@ -92,51 +94,52 @@ func Initialize(log *ipfsLog.ZapEventLogger, cfg config.Config) host.Host { //no
 	* RELAY SERVICE
 	 */
 	// Check if the node is behind NAT
-	// behindNAT := network.NATDiscovery(log)
+	behindNAT := network.NATDiscovery(log)
 
 	// If the node is behind NAT, search for a node that supports relay
 	// TODO: Optimize this code
-	// if !behindNAT {
-	// 	log.Debugln("Node is not behind NAT")
-	// Start the relay service
-	// _, err = relay.New(h) // relay.WithResources(relay.Resources{
-	// 	Limit: &relay.RelayLimit{
-	// 		// Duration is the time limit before resetting a relayed connection; defaults to 2min.
-	// 		Duration: cfg.MaxRelayedConnectionDuration,
-	// 		// Data is the limit of data relayed (on each direction) before resetting the connection.
-	// 		// Defaults to 128KB
-	// 		// Data: 1 << 30, // 1 GB
-	// 		Data: cfg.MaxDataRelayed,
-	// 	},
-	// 	// Default values
-	// 	// ReservationTTL is the duration of a new (or refreshed reservation).
-	// 	// Defaults to 1hr.
-	// 	ReservationTTL: cfg.RelayReservationTTL,
-	// 	// MaxReservations is the maximum number of active relay slots; defaults to 128.
-	// 	MaxReservations: cfg.MaxRelayReservations,
-	// 	// MaxCircuits is the maximum number of open relay connections for each peer; defaults to 16.
-	// 	MaxCircuits: cfg.MaxRelayCircuits,
-	// 	// BufferSize is the size of the relayed connection buffers; defaults to 2048.
-	// 	BufferSize: cfg.MaxRelayedConnectionBufferSize,
+	if !behindNAT {
+		log.Debugln("Node is not behind NAT")
+		// Start the relay service
+		_, err = relay.New(h,
+			relay.WithResources(relay.Resources{
+				Limit: &relay.RelayLimit{
+					// Duration is the time limit before resetting a relayed connection; defaults to 2min.
+					Duration: cfg.MaxRelayedConnectionDuration,
+					// Data is the limit of data relayed (on each direction) before resetting the connection.
+					// Defaults to 128KB
+					// Data: 1 << 30, // 1 GB
+					Data: cfg.MaxDataRelayed,
+				},
+				// Default values
+				// ReservationTTL is the duration of a new (or refreshed reservation).
+				// Defaults to 1hr.
+				ReservationTTL: cfg.RelayReservationTTL,
+				// MaxReservations is the maximum number of active relay slots; defaults to 128.
+				MaxReservations: cfg.MaxRelayReservations,
+				// MaxCircuits is the maximum number of open relay connections for each peer; defaults to 16.
+				MaxCircuits: cfg.MaxRelayCircuits,
+				// BufferSize is the size of the relayed connection buffers; defaults to 2048.
+				BufferSize: cfg.MaxRelayedConnectionBufferSize,
 
-	// 	// MaxReservationsPerPeer is the maximum number of reservations originating from the same
-	// 	// peer; default is 4.
-	// 	MaxReservationsPerPeer: cfg.MaxRelayReservationsPerPeer,
-	// 	// MaxReservationsPerIP is the maximum number of reservations originating from the same
-	// 	// IP address; default is 8.
-	// 	MaxReservationsPerIP: cfg.MaxRelayReservationsPerIP,
-	// 	// MaxReservationsPerASN is the maximum number of reservations origination from the same
-	// 	// ASN; default is 32
-	// 	MaxReservationsPerASN: cfg.MaxRelayReservationsPerASN,
-	// }),
-
-	// 	if err != nil {
-	// 		log.Errorln("Error instantiating relay service : ", err)
-	// 	}
-	// 	log.Debugln("Relay service started")
-	// } else {
-	// 	log.Debugln("Node is behind NAT")
-	// }
+				// MaxReservationsPerPeer is the maximum number of reservations originating from the same
+				// peer; default is 4.
+				MaxReservationsPerPeer: cfg.MaxRelayReservationsPerPeer,
+				// MaxReservationsPerIP is the maximum number of reservations originating from the same
+				// IP address; default is 8.
+				MaxReservationsPerIP: cfg.MaxRelayReservationsPerIP,
+				// MaxReservationsPerASN is the maximum number of reservations origination from the same
+				// ASN; default is 32
+				MaxReservationsPerASN: cfg.MaxRelayReservationsPerASN,
+			}),
+		)
+		if err != nil {
+			log.Errorln("Error instantiating relay service : ", err)
+		}
+		log.Debugln("Relay service started")
+	} else {
+		log.Debugln("Node is behind NAT")
+	}
 
 	log.Infof("Host protocols are: %v", h.Mux().Protocols())
 
