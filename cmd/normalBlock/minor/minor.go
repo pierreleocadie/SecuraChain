d6be083b74f688f1d36dc95eb63a90b49589d1fd
Jordan10980 <jordandohou11@gmail.com>
2024-03-05 22:12:34 +0100

    feat(second-test):  test the treatmeant of a normal block of which his previous block is stored

---
+++ cmd/normalBlock/minor/minor.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	ipfsLog "github.com/ipfs/go-log/v2"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/event"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/pierreleocadie/SecuraChain/internal/config"
+	"github.com/pierreleocadie/SecuraChain/internal/core/block"
+	"github.com/pierreleocadie/SecuraChain/internal/core/consensus"
+	"github.com/pierreleocadie/SecuraChain/internal/core/transaction"
+	netwrk "github.com/pierreleocadie/SecuraChain/internal/network"
+	"github.com/pierreleocadie/SecuraChain/internal/node"
+	"github.com/pierreleocadie/SecuraChain/pkg/ecdsa"
+	"github.com/pierreleocadie/SecuraChain/pkg/utils"
+)
+
+var yamlConfigFilePath = flag.String("config", "", "Path to the yaml config file")
+
+func main() {
+	log := ipfsLog.Logger("full-node")
+	if err := ipfsLog.SetLogLevel("full-node", "DEBUG"); err != nil {
+		log.Errorln("Failed to set log level : ", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	flag.Parse()
+
+	// Load the config file
+	if *yamlConfigFilePath == "" {
+		log.Errorln("Please provide a path to the yaml config file")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	cfg, err := config.LoadConfig(*yamlConfigFilePath)
+	if err != nil {
+		log.Errorln("Error loading config file : ", err)
+		os.Exit(1)
+	}
+
+	/*
+	* NODE LIBP2P
+	 */
+	// Initialize the node
+
+	host := node.Initialize(log, *cfg)
+	defer host.Close()
+	log.Debugf("Storage node initizalized with PeerID: %s", host.ID().String())
+
+	/*
+	 * NETWORK PEER DISCOVERY WITH mDNS
+	 */
+	// Initialize mDNS
+	mdnsConfig := netwrk.NewMDNSDiscovery(cfg.RendezvousStringFlag)
+	// Run MDNS
+	if err := mdnsConfig.Run(host); err != nil {
+		log.Fatalf("Failed to run mDNS: %s", err)
+		return
+	}
+
+	/*
+	* PUBSUB
+	 */
+	ps, err := pubsub.NewGossipSub(ctx, host)
+	if err != nil {
+		panic(err)
+	}
+
+	// Join the topic BlockAnnouncementStringFlag
+	blockAnnouncementTopic, err := ps.Join(cfg.BlockAnnouncementStringFlag)
+	if err != nil {
+		fmt.Println("Failed to join block announcement topic : %s\n", err)
+	}
+
+	minerKeyPair, _ := ecdsa.NewECDSAKeyPair()  // Replace with actual key pair generation
+	transactions := []transaction.Transaction{} // Empty transaction list for simplicity
+
+	/*
+	* FIRST BLOCK
+	 */
+
+	// Create a genesis block
+	genesisBlock := block.NewBlock(transactions, nil, 1, minerKeyPair)
+
+	consensus.MineBlock(genesisBlock)
+
+	key := block.ComputeHash(genesisBlock)
+
+	err = genesisBlock.SignBlock(minerKeyPair)
+	if err != nil {
+		fmt.Println("Error signing block: %v", err)
+	}
+
+	genesisBlockBytes, err := genesisBlock.Serialize()
+	if err != nil {
+		fmt.Println("Error serializing block: %v", err)
+	}
+
+	// Publish a message to ask for the blockchain
+	fmt.Println("Sending a genesis block to full node")
+	if err := blockAnnouncementTopic.Publish(ctx, genesisBlockBytes); err != nil {
+		fmt.Printf("Error publishing blockchain request : %s\n", err)
+	}
+
+	fmt.Println("Waiting 20 seconds to send the second block to the full node")
+	time.Sleep(20 * time.Second)
+	/*
+	* SECOND BLOCK
+	 */
+
+	// Create a second block
+	secondBlock := block.NewBlock(transactions, key, 2, minerKeyPair)
+
+	consensus.MineBlock(secondBlock)
+
+	err = secondBlock.SignBlock(minerKeyPair)
+	if err != nil {
+		fmt.Println("Error signing block: %v", err)
+	}
+
+	// Check if the returned block is not nil
+	if secondBlock == nil {
+		fmt.Println("NewBlock returned nil")
+	}
+
+	secondBlockBytes, err := secondBlock.Serialize()
+	if err != nil {
+		fmt.Println("Error serializing block: %v", err)
+	}
+
+	// Publish a message to ask for the blockchain
+	fmt.Println("Sending a second block to full node")
+	if err := blockAnnouncementTopic.Publish(ctx, secondBlockBytes); err != nil {
+		fmt.Printf("Error publishing blockchain request : %s\n", err)
+	}
+
+	/*
+	* DISPLAY PEER CONNECTEDNESS CHANGES
+	 */
+	// Subscribe to EvtPeerConnectednessChanged events
+	subNet, err := host.EventBus().Subscribe(new(event.EvtPeerConnectednessChanged))
+	if err != nil {
+		log.Errorln("Failed to subscribe to EvtPeerConnectednessChanged:", err)
+	}
+	defer subNet.Close()
+
+	// Handle connection events in a separate goroutine
+	go func() {
+		for e := range subNet.Out() {
+			evt := e.(event.EvtPeerConnectednessChanged)
+			if evt.Connectedness == network.Connected {
+				log.Debugln("Peer connected:", evt.Peer)
+			} else if evt.Connectedness == network.NotConnected {
+				log.Debugln("Peer disconnected:", evt.Peer)
+			}
+		}
+	}()
+
+	utils.WaitForTermSignal()
+
+}
