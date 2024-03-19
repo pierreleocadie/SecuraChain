7d02f401df44136781f64792a23667f72759ae93
Jordan10980 <jordandohou11@gmail.com>
2024-03-19 08:39:18 +0100

    chore : adding log and docs for receive_blocks.go

--- internal/fullnode/receive_blocks.go
+++ internal/fullnode/receive_blocks.go
@@ -4,23 +4,25 @@ import (
 	"context"
 	"fmt"
 
+	ipfsLog "github.com/ipfs/go-log/v2"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/pierreleocadie/SecuraChain/internal/core/block"
 )
 
-func ReceiveBlock(ctx context.Context, subBlockAnnouncement *pubsub.Subscription) (*block.Block, error) {
+// ReceiveBlock receives a block announcement message and deserializes it into a block.
+func ReceiveBlock(log *ipfsLog.ZapEventLogger, ctx context.Context, subBlockAnnouncement *pubsub.Subscription) (*block.Block, error) {
 	msg, err := subBlockAnnouncement.Next(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("error getting block announcement message : %s", err)
+		return nil, fmt.Errorf("error getting block announcement message: %s", err)
 	}
 
-	// fmt.Println("Received block announcement message from ", msg.GetFrom().String())
-	// fmt.Println("Received block : ", msg.Data)
+	log.Debugln("Received block announcement message from ", msg.GetFrom().String())
+	log.Debugln("Received block: ", msg.Data)
 
 	// Deserialize the block announcement
-	blockAnnounced, err := block.DeserializeBlock(msg.Data)
+	b, err := block.DeserializeBlock(msg.Data)
 	if err != nil {
-		return nil, fmt.Errorf("error deserializing block announcement : %s", err)
+		return nil, fmt.Errorf("error deserializing block announcement: %s", err)
 	}
-	return blockAnnounced, nil
+	return b, nil
 }
