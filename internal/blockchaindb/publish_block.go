3b29684e07e6de495b17ac05e6cb8848948b655d
Jordan10980 <jordandohou11@gmail.com>
2024-03-19 09:11:16 +0100

    chore: deleting publish_block.go, and add docs and logs for publish_block.go

--- internal/blockchaindb/publish_block.go
+++ internal/blockchaindb/publish_block.go
@@ -6,6 +6,7 @@ import (
 
 	files "github.com/ipfs/boxo/files"
 	"github.com/ipfs/boxo/path"
+	ipfsLog "github.com/ipfs/go-log/v2"
 	"github.com/ipfs/kubo/core"
 	icore "github.com/ipfs/kubo/core/coreiface"
 	"github.com/pierreleocadie/SecuraChain/internal/config"
@@ -13,51 +14,47 @@ import (
 	"github.com/pierreleocadie/SecuraChain/internal/ipfs"
 )
 
-// PublishBlockchainToIPFS adds the local blockchain to IPFS for synchronization and re-synchronization of nodes in case of absence.
-func PublishBlockToIPFS(ctx context.Context, config *config.Config, nodeIpfs *core.IpfsNode, ipfsApi icore.CoreAPI, b *block.Block) error {
-	// Add the blockchain to IPFS
-	fileImmutablePathCid, err := addBlockToIPFS(ctx, ipfsApi, b)
+// PublishBlockToIPFS publishes a block to IPFS and adds its metadata to the registry.
+func PublishBlockToIPFS(log *ipfsLog.ZapEventLogger, ctx context.Context, config *config.Config, nodeIpfs *core.IpfsNode, ipfsApi icore.CoreAPI, b *block.Block) error {
+	// Add the block to IPFS
+	cidFile, err := addBlockToIPFS(log, ctx, ipfsApi, b)
 	if err != nil {
-		fmt.Printf("Error adding the block to IPFS : %s\n ", err)
-		return err
+		return fmt.Errorf("error adding the block to IPFS : %s", err)
 	}
 
 	// Pin the file on IPFS
-	pinned, err := ipfs.PinFile(ctx, ipfsApi, fileImmutablePathCid)
+	_, err = ipfs.PinFile(ctx, ipfsApi, cidFile)
 	if err != nil {
-		fmt.Printf("Error pinning the block to IPFS : %s\n", err)
-		return err
+		return fmt.Errorf("error pinning the block to IPFS : %s", err)
 	}
-	fmt.Println("block pinned on IPFS : ", pinned)
+	log.Debugln("block pinned on IPFS")
 
+	// Get the node ID
 	nodeId := nodeIpfs.Peerstore.PeerInfo(nodeIpfs.Identity)
 
-	fmt.Println("[PublishBlockTo IPFS] Block added : ", fileImmutablePathCid)
-	// Save the file CID to the blockchain registry
-	if err := AddBlockMetadataToRegistry(b, config, fileImmutablePathCid, nodeId); err != nil {
-		fmt.Printf("Error adding the block metadata to the registry : %s\n", err)
-		return err
+	// Save the block metadata to the registry
+	if err := AddBlockMetadataToRegistry(b, config, cidFile, nodeId); err != nil {
+		return fmt.Errorf("error adding the block metadata to the registry : %s", err)
 	}
 
+	log.Debugln("block metadata added to the registry")
 	return nil
 }
 
-// AddFileToIPFS adds a file to IPFS and returns its CID. It also collects and saves file metadata.
-// The function handles the file addition process and records metadata such as file size, type, name, and user public key.
-func addBlockToIPFS(ctx context.Context, ipfsAPI icore.CoreAPI, b *block.Block) (path.ImmutablePath, error) {
+// addBlockToIPFS serializes the given block and adds it to IPFS.
+func addBlockToIPFS(log *ipfsLog.ZapEventLogger, ctx context.Context, ipfsAPI icore.CoreAPI, b *block.Block) (path.ImmutablePath, error) {
 	blockBytes, err := b.Serialize()
 	if err != nil {
 		return path.ImmutablePath{}, fmt.Errorf("could not serialize block: %s", err)
 	}
+	log.Debugln("Block serialized")
 
-	peerCidFile, err := ipfsAPI.Unixfs().Add(ctx,
+	cidFile, err := ipfsAPI.Unixfs().Add(ctx,
 		files.NewBytesFile(blockBytes))
 	if err != nil {
 		return path.ImmutablePath{}, fmt.Errorf("could not add File: %s", err)
 	}
 
-	fmt.Printf("Added file to peer with CID %s\n", peerCidFile.String())
-
-	return peerCidFile, nil
-
+	log.Debugln("Block added to IPFS with CID: ", cidFile.String())
+	return cidFile, nil
 }
