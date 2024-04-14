37f67169a3943d94f3216ea9814f3dae53f737be
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-04-14 14:44:19 +0200

    init mining node

---
+++ cmd/securachain-mining-node/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"flag"
+
+	ipfsLog "github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p/core/event"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/pierreleocadie/SecuraChain/internal/config"
+	"github.com/pierreleocadie/SecuraChain/internal/node"
+	"github.com/pierreleocadie/SecuraChain/pkg/utils"
+)
+
+var yamlConfigFilePath = flag.String("config", "", "Path to the yaml config file")
+
+func main() { //nolint: funlen
+	log := ipfsLog.Logger("mining-node")
+	err := ipfsLog.SetLogLevel("*", "DEBUG")
+	if err != nil {
+		log.Errorln("Error setting log level : ", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	flag.Parse()
+
+	// Load the config file
+	if *yamlConfigFilePath == "" {
+		log.Panicln("Please provide a path to the yaml config file. Flag: -config <path/to/config.yaml>")
+	}
+
+	cfg, err := config.LoadConfig(*yamlConfigFilePath)
+	if err != nil {
+		log.Panicln("Error loading config file : ", err)
+	}
+
+	/*
+	* NODE LIBP2P
+	 */
+	// Initialize the node
+	host := node.Initialize(log, *cfg)
+	defer host.Close()
+
+	/*
+	* DHT DISCOVERY
+	 */
+	// Setup DHT discovery
+	node.SetupDHTDiscovery(ctx, cfg, host, true, log)
+
+	/*
+	* DISPLAY PEER CONNECTEDNESS CHANGES
+	 */
+	// Subscribe to EvtPeerConnectednessChanged events
+	subNet, err := host.EventBus().Subscribe(new(event.EvtPeerConnectednessChanged))
+	if err != nil {
+		log.Debugln("Failed to subscribe to EvtPeerConnectednessChanged: ", err)
+	}
+	defer subNet.Close()
+
+	// Handle connection events in a separate goroutine
+	go func() {
+		for e := range subNet.Out() {
+			evt := e.(event.EvtPeerConnectednessChanged)
+			if evt.Connectedness == network.Connected {
+				log.Debugln("Peer connected:", evt.Peer)
+			} else if evt.Connectedness == network.NotConnected {
+				log.Debugln("Peer disconnected: ", evt.Peer)
+			}
+		}
+	}()
+
+	// Wait for a termination signal
+	utils.WaitForTermSignal()
+}
