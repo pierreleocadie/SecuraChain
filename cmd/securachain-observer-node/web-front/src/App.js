4b43a230ae08738d373b28064ee890f07b7432da
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-04-27 17:00:25 +0200

    refactor(App.js): refactor App component to use react-router for navigation and clean up commented out code

--- cmd/securachain-observer-node/web-front/src/App.js
+++ cmd/securachain-observer-node/web-front/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import BlockchainData from './BlockchainData';
 import NetworkData from './NetworkData';
 
 const App = () => {
     return (
-        <div className="App">
-            <NetworkData/>
-        </div>
+        <BrowserRouter>
+            <div>
+                <nav>
+                    <Link to="/">Network Visualisation</Link>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <Link to="/blockchain">Blockchain Visualisation</Link>
+                </nav>
+                <Routes>
+                    <Route path="/" element={<NetworkData />} />
+                    <Route path="/blockchain" element={<BlockchainData />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
+    // <div className="App">
+    //     <NetworkData/>
+    // </div>
 };
 
 export default App;
