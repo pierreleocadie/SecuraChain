7845a4ac609aecfb694f8f3b5b6ab035e486b11b
Jordan10980 <jordandohou11@gmail.com>
2024-06-02 16:59:32 +0200

    refactor(ipfs): Remove unused GetDirectoryWithPath function

--- internal/ipfs/get_file.go
+++ internal/ipfs/get_file.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"sync"
 
 	files "github.com/ipfs/boxo/files"
 	"github.com/ipfs/boxo/path"
@@ -47,59 +46,6 @@ func GetFile(ctx context.Context, config *config.Config, ipfsAPI icore.CoreAPI, 
 
 	// Print confirmation message indicating the file has been fetched and saved.
 	log.Printf("Got file back from IPFS (IPFS path: %s) and wrote it to %s\n", cidFile.String(), downloadedFilePath)
-
-	return nil
-}
-
-// GetDirectoryWithPath download a directory using its CID (Content Identifier).
-// It creates the necessary directory if it doesn't exist and writes the file to a specified path.
-func GetDirectoryWithPath(ctx context.Context, ipfsAPI icore.CoreAPI, directoryFile path.ImmutablePath, downloadPath string) error {
-	local, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
-	downloadsStoragePath := filepath.Join(local, downloadPath)
-	// Ensure the output directory exists or create it.
-	if err := os.MkdirAll(downloadsStoragePath, 0700); err != nil {
-		return fmt.Errorf("error creating output directory: %v", err)
-	}
-
-	log.Printf("Getting file with CID: %s\n", directoryFile.String())
-
-	rootNodeFile, err := ipfsAPI.Unixfs().Get(ctx, directoryFile)
-	if err != nil {
-		return fmt.Errorf("could not get directory with CID: %s", err)
-	}
-	defer rootNodeFile.Close()
-	log.Printf("Got the directory")
-
-	downloadedFilePath := filepath.Join(downloadPath, filepath.Base(directoryFile.String()))
-
-	var wg sync.WaitGroup
-	errCh := make(chan error, 1) // create an error channel
-	log.Printf("Writing file to %s\n", downloadedFilePath)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		err = files.WriteTo(rootNodeFile, downloadedFilePath)
-		if err != nil {
-			errCh <- fmt.Errorf("could not write out the fetched CID: %v", err) // send error to channel
-			return
-		}
-	}()
-
-	wg.Wait()
-
-	close(errCh) // close the channel
-
-	// check if there was an error
-	if err := <-errCh; err != nil {
-		fmt.Println(err)
-	}
-
-	// Print confirmation message indicating the directory has been fetched and saved.
-	log.Printf("Got directory back from IPFS (IPFS path: %s) and wrote it to %s\n", directoryFile.String(), downloadedFilePath)
 
 	return nil
 }
