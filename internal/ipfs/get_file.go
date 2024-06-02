ea255726ef1c362e3319bb9812d89cb4075e4f02
Jordan10980 <jordandohou11@gmail.com>
2024-06-02 17:14:14 +0200

    refactor(ipfs): Update GetFile function to return the downloaded file only and just get the file from IPFS

--- internal/ipfs/get_file.go
+++ internal/ipfs/get_file.go
@@ -5,8 +5,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
-	"path/filepath"
 
 	files "github.com/ipfs/boxo/files"
 	"github.com/ipfs/boxo/path"
@@ -15,37 +13,15 @@ import (
 )
 
 // GetFile download a file using its CID (Content Identifier).
-// It creates the necessary directory if it doesn't exist and writes the file to a specified path.
-func GetFile(ctx context.Context, config *config.Config, ipfsAPI icore.CoreAPI, cidFile path.ImmutablePath) error {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return err
-	}
-
-	downloadsStoragePath := filepath.Join(home, ".IPFS_Downloads/")
-	// Ensure the output directory exists or create it.
-	if err := os.MkdirAll(downloadsStoragePath, os.FileMode(config.FileRights)); err != nil {
-		return fmt.Errorf("error creating output directory: %v", err)
-	}
-
+func GetFile(ctx context.Context, config *config.Config, ipfsAPI icore.CoreAPI, cidFile path.ImmutablePath) (files.Node, error) {
 	log.Printf("Getting file with CID: %s\n", cidFile.String())
 	rootNodeFile, err := ipfsAPI.Unixfs().Get(ctx, cidFile)
 	if err != nil {
-		return fmt.Errorf("could not get file with CID: %s", err)
+		return nil, fmt.Errorf("could not get the file: %v", err)
 	}
 	defer rootNodeFile.Close()
-	log.Printf("Got the file")
 
-	downloadedFilePath := filepath.Join(downloadsStoragePath, filepath.Base(cidFile.String()))
+	log.Printf("Got file back from IPFS (IPFS path: %s)", cidFile.String())
 
-	log.Printf("Writing file to %s\n", downloadedFilePath)
-	err = files.WriteTo(rootNodeFile, downloadedFilePath)
-	if err != nil {
-		return fmt.Errorf("could not write out the fetched CID: %v", err) // send error to channel
-	}
-
-	// Print confirmation message indicating the file has been fetched and saved.
-	log.Printf("Got file back from IPFS (IPFS path: %s) and wrote it to %s\n", cidFile.String(), downloadedFilePath)
-
-	return nil
+	return rootNodeFile, nil
 }
