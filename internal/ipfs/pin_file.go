0d4fbd668626a8c18873fd6786dd7adbec9e2000
Jordan10980 <jordandohou11@gmail.com>
2024-06-02 16:32:51 +0200

    refactor(ipfs):UnpinFile returning only an error

--- internal/ipfs/pin_file.go
+++ internal/ipfs/pin_file.go
@@ -12,13 +12,13 @@ import (
 func PinFile(ctx context.Context, ipfsAPI icore.CoreAPI, fileCid path.ImmutablePath) error {
 	if err := ipfsAPI.Pin().Add(ctx, fileCid); err != nil {
 		log.Printf("Could not pin file %v", err)
-		return fmt.Errorf("Could not pin file %v", err)
+		return fmt.Errorf("could not pin file %v", err)
 	}
 
 	_, IsPinned, err := ipfsAPI.Pin().IsPinned(ctx, fileCid)
 	if err != nil {
 		log.Printf("Could not check if file is pinned %v", err)
-		return fmt.Errorf("Could not check if file is pinned %v", err)
+		return fmt.Errorf("could not check if file is pinned %v", err)
 	}
 
 	log.Printf("File pinned: %v", IsPinned)
--- internal/ipfs/unpin_file.go
+++ internal/ipfs/unpin_file.go
@@ -2,24 +2,25 @@ package ipfs
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/ipfs/boxo/path"
 	icore "github.com/ipfs/kubo/core/coreiface"
 )
 
-func UnpinFile(ctx context.Context, ipfsAPI icore.CoreAPI, fileCid path.ImmutablePath) (bool, error) {
+func UnpinFile(ctx context.Context, ipfsAPI icore.CoreAPI, fileCid path.ImmutablePath) error {
 	if err := ipfsAPI.Pin().Rm(ctx, fileCid); err != nil {
 		log.Printf("Could not unpin file %v", err)
-		return false, err
+		return fmt.Errorf("could not unpin file %v", err)
 	}
 
 	_, IsUnpinned, err := ipfsAPI.Pin().IsPinned(ctx, fileCid)
 	if err != nil {
 		log.Printf("Could not check if file is unpinned %v", err)
-		return IsUnpinned, err
+		return fmt.Errorf("could not check if file is unpinned %v", err)
 	}
 
 	log.Printf("File unpinned: %v", IsUnpinned)
-	return IsUnpinned, nil
+	return nil
 }
