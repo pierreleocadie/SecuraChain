70ae9af03a3b161347207b45b4cba04e07d2b27d
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-04 11:41:04 +0200

    refactor(signature.go): change VerifyBlock function parameter from pointer to value type for consistency and to improve code readability

--- internal/core/block/signature.go
+++ internal/core/block/signature.go
@@ -7,7 +7,7 @@ import (
 )
 
 // VerifyBlock checks if the block signature is valid
-func VerifyBlock(currentBlock *Block) bool {
+func VerifyBlock(currentBlock Block) bool {
 	if len(currentBlock.Header.Signature) == 0 {
 		log.Printf("Block validation failed: Signature is empty")
 		return false
