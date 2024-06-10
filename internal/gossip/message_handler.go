7843e6ae8119f884a0deba14f3c412f7b488e61c
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-11 00:03:06 +0200

    chore(gossip): remove the unsed package

--- internal/gossip/message_handler.go
+++
@@ -1,8 +0,0 @@
-package gossip
-
-import pubsub "github.com/libp2p/go-libp2p-pubsub"
-
-// MessageHandler interface pour le traitement des messages
-type MessageHandler interface {
-	HandleMessage(msg *pubsub.Message)
-}
