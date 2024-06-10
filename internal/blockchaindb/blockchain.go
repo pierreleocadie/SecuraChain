75abcc0fbf343be798fa777ac5caf008d4e42141
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-11 00:03:28 +0200

    refactor(blockchain.go): simplify Blockchain interface method signatures for better readability and maintainability

--- internal/blockchaindb/blockchain.go
+++ internal/blockchaindb/blockchain.go
@@ -1,15 +1,14 @@
 package blockchaindb
 
 import (
-	ipfsLog "github.com/ipfs/go-log/v2"
 	"github.com/pierreleocadie/SecuraChain/internal/core/block"
 )
 
 // Blockchain represents an interface for interacting with a blockchain.
 type Blockchain interface {
-	SaveBlock(log *ipfsLog.ZapEventLogger, key []byte, b *block.Block) error
-	GetBlock(log *ipfsLog.ZapEventLogger, key []byte) (*block.Block, error)
-	VerifyIntegrity(log *ipfsLog.ZapEventLogger) bool
-	GetLastBlock(log *ipfsLog.ZapEventLogger) *block.Block
-	Close(log *ipfsLog.ZapEventLogger) error
+	AddBlock(b block.Block) error
+	GetBlock(key []byte) (block.Block, error)
+	VerifyIntegrity() error
+	GetLastBlock() block.Block
+	Close() error
 }
