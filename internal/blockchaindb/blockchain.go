83c546d685ae496d83a1668c53479b6006ee4ab7
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-11 00:19:53 +0200

    refactor(blockchain.go): rename Blockchain interface to BlockchainDB for clarity and consistency with the file and package name

--- internal/blockchaindb/blockchain.go
+++ internal/blockchaindb/blockchain.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Blockchain represents an interface for interacting with a blockchain.
-type Blockchain interface {
+type BlockchainDB interface {
 	AddBlock(b block.Block) error
 	GetBlock(key []byte) (block.Block, error)
 	VerifyIntegrity() error
