150f88571e9f130f84280fb1fe3fc0690b448f4a
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-11 00:04:40 +0200

    refactor(pebbledb.go): improve code maintanability

--- internal/blockchaindb/pebbledb.go
+++ internal/blockchaindb/pebbledb.go
@@ -11,39 +11,125 @@ import (
 
 // PebbleDB wraps a Pebble database instance to store blockchain data.
 type PebbleDB struct {
-	db *pebble.DB
+	db  *pebble.DB
+	log *ipfsLog.ZapEventLogger
 }
 
 // BlockchainDB wraps a Pebble database instance to store blockchain data.
-func NewBlockchainDB(log *ipfsLog.ZapEventLogger, dbPath string) (*PebbleDB, error) {
+func NewBlockchainDB(logger *ipfsLog.ZapEventLogger, dbPath string) (*PebbleDB, error) {
 	db, err := pebble.Open(dbPath,
 		&pebble.Options{
-			Logger: log,
+			Logger: logger,
 		},
 	)
 	if err != nil {
-		log.Errorln("failed to open pebble database")
 		return nil, fmt.Errorf("failed to open pebble database : %v", err)
 	}
-	log.Debugln("Pebble database opened successfully")
-	return &PebbleDB{db: db}, nil
+	logger.Info("Pebble database opened successfully")
+	return &PebbleDB{db: db, log: logger}, nil
 }
 
-// SaveBlock serializes and stores a given block in the database.
-func (pdb *PebbleDB) SaveBlock(log *ipfsLog.ZapEventLogger, key []byte, b *block.Block) error {
+// AddBlockToBlockchain adds a block to the blockchain database.
+func (pdb *PebbleDB) AddBlock(b block.Block) error {
+	if block.IsGenesisBlock(b) {
+		return pdb.addGenesisBlock(b)
+	}
+	return pdb.addNormalBlock(b)
+}
+
+// GetBlock retrieves a block from the database using its key.
+func (pdb *PebbleDB) GetBlock(key []byte) (block.Block, error) {
+	blockData, closer, err := pdb.db.Get(key)
+	if err != nil {
+		if err == pebble.ErrNotFound {
+			pdb.log.Warn("block not exists in the database")
+			return block.Block{}, pebble.ErrNotFound
+		}
+		return block.Block{}, fmt.Errorf("error retrieving block : %v", err)
+	}
+	defer closer.Close()
+
+	b, err := block.DeserializeBlock(blockData)
+	if err != nil {
+		return block.Block{}, fmt.Errorf("error deserializing block: %v", err)
+	}
+
+	pdb.log.Debugln("block retrieved successfully")
+	return b, nil
+}
+
+// VerifyIntegrity verifies the integrity of the blockchain stored in the BlockchainDB based on the last block.
+func (pdb *PebbleDB) VerifyIntegrity() error {
+	pdb.log.Infoln("Verifying blockchain integrity")
+
+	// Retrieve the last block from the database
+	lastBlock, err := pdb.GetLastBlock()
+	if err != nil {
+		return fmt.Errorf("error retrieving last block: %v", err)
+	}
+
+	// initialize the block key to the previous block of the last block
+	blockKey := lastBlock.PrevBlock
+
+	// Loop through the blockchain to verify the integrity
+	for blockKey != nil {
+		currentBlock, err := pdb.GetBlock(blockKey)
+		if err != nil {
+			return fmt.Errorf("error retrieving block: %v", err)
+		}
+
+		if block.IsGenesisBlock(currentBlock) {
+			if err := consensus.ValidateBlock(currentBlock, block.Block{}); err != nil {
+				return fmt.Errorf("block validation failed: %v", err)
+			}
+			break
+		}
+
+		prevBlock, err := pdb.GetBlock(currentBlock.PrevBlock)
+		if err != nil {
+			return fmt.Errorf("previous block not found: %v", err)
+		}
+
+		if err := consensus.ValidateBlock(currentBlock, prevBlock); err != nil {
+			return fmt.Errorf("block validation failed: %v", err)
+		}
+
+		blockKey = currentBlock.PrevBlock
+	}
+
+	pdb.log.Infoln("Blockchain integrity verified")
+	return nil
+}
+
+// GetLastBlock retrieves the most recently added block from the database.
+func (pdb *PebbleDB) GetLastBlock() (block.Block, error) {
+	lastBlock, err := pdb.GetBlock([]byte("lastBlockKey"))
+	if err != nil {
+		return block.Block{}, fmt.Errorf("error retrieving last block: %v", err)
+	}
+	pdb.log.Debugln("last block retrieved successfully")
+	return lastBlock, nil
+}
+
+// Close closes the database connection.
+func (pdb *PebbleDB) Close() error {
+	pdb.log.Infoln("closing pebble database")
+	return pdb.db.Close()
+}
+
+// saveBlock serializes and stores a given block in the database.
+func (pdb *PebbleDB) saveBlock(key []byte, b block.Block) error {
 	blockBytes, err := b.Serialize()
 	if err != nil {
-		log.Errorln("error serializing block")
 		return fmt.Errorf("error serializing block: %v", err)
 	}
 
 	// Size of the serialized block
 	serializedBlockSize := float64(len(blockBytes)) / 1024
 
-	log.Infof("Block size: %.2f KB", serializedBlockSize)
+	pdb.log.Debugf("Block size: %.2f KB", serializedBlockSize)
 	err = pdb.db.Set(key, blockBytes, nil)
 	if err != nil {
-		log.Errorln("error saving block to the database")
 		return fmt.Errorf("error saving block to the database : %v", err)
 	}
 
@@ -51,92 +137,52 @@ func (pdb *PebbleDB) SaveBlock(log *ipfsLog.ZapEventLogger, key []byte, b *block
 
 	// Delete the previous reference to the last block
 	if err := pdb.db.Delete(key, nil); err != nil {
-		log.Errorln("error deleting last block reference")
 		return fmt.Errorf("error deleting last block reference: %v", err)
 	}
 
 	if err := pdb.db.Set(key, blockBytes, nil); err != nil {
-		log.Errorln("error updating last block reference")
 		return fmt.Errorf("error updating last block reference: %v", err)
 	}
 
-	log.Infoln("block saved successfully")
+	pdb.log.Infoln("block saved successfully")
 	return nil
 }
 
-// GetBlock retrieves a block from the database using its key.
-func (pdb *PebbleDB) GetBlock(log *ipfsLog.ZapEventLogger, key []byte) (*block.Block, error) {
-	blockData, closer, err := pdb.db.Get(key)
+// addGenesisBlock adds the genesis block to the blockchain database.
+func (pdb *PebbleDB) addGenesisBlock(b block.Block) error {
+	// Compute the hash of the block to use as a key in the db
+	key := block.ComputeHash(b)
+
+	err := pdb.saveBlock(key, b)
+	if err != nil {
+		return fmt.Errorf("failed to add genesis block to the db: %s", err)
+	}
+
+	pdb.log.Debugln("Genesis block successfully added to the blockchain")
+	return nil
+}
+
+// addNormalBlock adds a normal block to the blockchain database.
+func (pdb *PebbleDB) addNormalBlock(b block.Block) error {
+	key := block.ComputeHash(b)
+
+	// Check if the block already exists in the db
+	_, err := pdb.GetBlock(key)
 	if err != nil {
 		if err == pebble.ErrNotFound {
-			log.Warn("block not exists in the database")
-			return nil, pebble.ErrNotFound
-		}
-		log.Errorln("error retrieving block")
-		return nil, fmt.Errorf("error retrieving block : %v", err)
-	}
-	defer closer.Close()
+			pdb.log.Warnf("Block not found in the db: %s\n", err)
 
-	b, err := block.DeserializeBlock(blockData)
-	if err != nil {
-		log.Errorln("error deserializing block")
-		return nil, fmt.Errorf("error deserializing block: %v", err)
-	}
-
-	log.Debugln("block retrieved successfully")
-	return b, nil
-}
-
-// VerifyIntegrity verifies the integrity of the blockchain stored in the BlockchainDB based on the last block.
-func (pdb *PebbleDB) VerifyIntegrity(log *ipfsLog.ZapEventLogger) bool {
-	log.Infoln("Verifying blockchain integrity")
-
-	for blockKey := pdb.GetLastBlock(log).PrevBlock; blockKey != nil; {
-		currentBlock, err := pdb.GetBlock(log, blockKey)
-		if err != nil {
-			log.Errorln("error retrieving block")
-			return false
-		}
-
-		if block.IsGenesisBlock(currentBlock) {
-			if !consensus.ValidateBlock(currentBlock, nil) {
-				log.Errorln("block validation failed")
-				return false
+			if err := pdb.saveBlock(key, b); err != nil {
+				return fmt.Errorf("failed to add block to the db: %s", err)
 			}
-			break
+
+			pdb.log.Debugln("Block successfully added to the blockchain")
+			return nil
 		}
 
-		prevBlock, err := pdb.GetBlock(log, currentBlock.PrevBlock)
-		if err != nil {
-			log.Errorln("previous block not found")
-			return false
-		}
+		return fmt.Errorf("failed to retrieve block from the db: %s", err)
 
-		if !consensus.ValidateBlock(currentBlock, prevBlock) {
-			log.Errorln("block validation failed")
-			return false
-		}
-
-		blockKey = currentBlock.PrevBlock
 	}
 
-	log.Infoln("Blockchain integrity verified")
-	return true
-}
-
-// GetLastBlock retrieves the most recently added block from the database.
-func (pdb *PebbleDB) GetLastBlock(log *ipfsLog.ZapEventLogger) *block.Block {
-	lastBlock, err := pdb.GetBlock(log, []byte("lastBlockKey"))
-	if err != nil {
-		log.Errorln("error retrieving last block")
-		return nil
-	}
-	log.Debugln("last block retrieved successfully")
-	return lastBlock
-}
-
-// Close closes the database connection.
-func (pdb *PebbleDB) Close(log *ipfsLog.ZapEventLogger) error {
-	log.Infoln("closing pebble database")
-	return pdb.db.Close()
+	return fmt.Errorf("block already exists in the blockchain")
 }
