a0781dea1e7a45177e4a7ee05b6fcdeb84e91335
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-11 12:20:03 +0200

    refactor(block_validation.go): restructure block validation logic to use interfaces and improve code readability and maintainability

--- internal/core/consensus/block_validation.go
+++ internal/core/consensus/block_validation.go
@@ -11,24 +11,40 @@ import (
 	"github.com/pierreleocadie/SecuraChain/internal/core/block"
 )
 
+type BlockValidator interface {
+	Validate(currentBlock, prevBlock block.Block) error
+}
+
+type GenesisBlockValidator interface {
+	Validate(genesisBlock block.Block) error
+}
+
+type DefaultBlockValidator struct {
+	genesisValidator GenesisBlockValidator
+}
+
+func NewDefaultBlockValidator(genesisValidator GenesisBlockValidator) *DefaultBlockValidator {
+	return &DefaultBlockValidator{genesisValidator: genesisValidator}
+}
+
 // ValidateBlock validates the given block
-func ValidateBlock(currentBlock, prevBlock block.Block) error {
+func (v DefaultBlockValidator) Validate(currentBlock, prevBlock block.Block) error {
 	if reflect.DeepEqual(currentBlock, block.Block{}) {
-		return fmt.Errorf("Block validation failed: current block is empty")
+		return fmt.Errorf("block validation failed: current block is empty")
 	}
 
 	// Special handling for the genesis block
-	if currentBlock.Header.Height == 1 && reflect.DeepEqual(prevBlock, block.Block{}) {
-		return validateGenesisBlock(currentBlock)
+	if currentBlock.Header.Height == 1 {
+		return v.genesisValidator.Validate(currentBlock)
 	}
 
 	if reflect.DeepEqual(prevBlock, block.Block{}) {
-		return fmt.Errorf("Block validation failed: previous block is empty")
+		return fmt.Errorf("block validation failed: previous block is empty")
 	}
 
 	// Check if the block's previous hash matches the hash of the previous block
 	if currentBlock.Header.Height > 1 && !bytes.Equal(currentBlock.Header.PrevBlock, block.ComputeHash(prevBlock)) {
-		return fmt.Errorf("Block validation failed: Previous hash does not match")
+		return fmt.Errorf("block validation failed: Previous hash does not match")
 	}
 
 	// Verify the block hash meets the difficulty requirement
@@ -39,22 +55,22 @@ func ValidateBlock(currentBlock, prevBlock block.Block) error {
 	hashInt.SetBytes(hash)
 
 	if hashInt.Cmp(target) == 1 {
-		return fmt.Errorf("Block validation failed: Hash does not meet the target")
+		return fmt.Errorf("block validation failed: Hash does not meet the target")
 	}
 
 	// Verify the block's timestamp is not too far in the future
 	if currentBlock.Header.Timestamp > time.Now().UTC().Unix()+10 {
-		return fmt.Errorf("Block validation failed: Timestamp is too far in the future")
+		return fmt.Errorf("block validation failed: Timestamp is too far in the future")
 	}
 
 	// Verify the block's timestamp is not before the previous block's timestamp
 	if currentBlock.Header.Timestamp < prevBlock.Header.Timestamp {
-		return fmt.Errorf("Block validation failed: Timestamp is before the previous block's timestamp")
+		return fmt.Errorf("block validation failed: Timestamp is before the previous block's timestamp")
 	}
 
 	// Verify the block's signature is valid
 	if err := block.VerifyBlock(currentBlock); err != nil {
-		return fmt.Errorf("Block validation failed: Signature is invalid: %w", err)
+		return fmt.Errorf("block validation failed: Signature is invalid: %w", err)
 	}
 
 	// If not empty, verify the block's transactions
@@ -62,7 +78,7 @@ func ValidateBlock(currentBlock, prevBlock block.Block) error {
 		for _, tx := range currentBlock.Transactions {
 			if err := ValidateTransaction(tx); err != nil {
 				log.Printf("Block validation failed: Transaction is invalid")
-				return fmt.Errorf("Block validation failed: Transaction is invalid")
+				return fmt.Errorf("block validation failed: Transaction is invalid")
 			}
 		}
 	}
@@ -70,8 +86,9 @@ func ValidateBlock(currentBlock, prevBlock block.Block) error {
 	return nil
 }
 
-// ValidateGenesisBlock validates the given genesis block
-func validateGenesisBlock(genesisBlock block.Block) error {
+type DefaultGenesisBlockValidator struct{}
+
+func (v DefaultGenesisBlockValidator) Validate(genesisBlock block.Block) error {
 	// Verify the block hash meets the difficulty requirement
 	target := big.NewInt(1)
 	target.Lsh(target, uint(sha256bits-genesisBlock.Header.TargetBits))
@@ -80,27 +97,29 @@ func validateGenesisBlock(genesisBlock block.Block) error {
 	hashInt.SetBytes(hash)
 
 	if hashInt.Cmp(target) == 1 {
-		log.Printf("Block validation failed: Hash does not meet the target")
-		return fmt.Errorf("Block validation failed: Hash does not meet the target")
+		return fmt.Errorf("block validation failed: Hash does not meet the target")
 	}
 
 	// Verify the block's timestamp is not too far in the future
 	if genesisBlock.Header.Timestamp > time.Now().UTC().Unix()+10 {
-		log.Printf("Block validation failed: Timestamp is too far in the future")
-		return fmt.Errorf("Block validation failed: Timestamp is too far in the future")
+		return fmt.Errorf("block validation failed: Timestamp is too far in the future")
 	}
 
 	// Verify the block's height is equal to one (genesis block)
 	if genesisBlock.Header.Height != 1 {
-		log.Printf("Block validation failed: Height is not one")
-		return fmt.Errorf("Block validation failed: Height is not one")
+		return fmt.Errorf("block validation failed: Height is not one")
 	}
 
 	// Verify the block's signature is valid
 	if err := block.VerifyBlock(genesisBlock); err != nil {
-		log.Printf("Block validation failed: Signature is invalid")
-		return fmt.Errorf("Block validation failed: Signature is invalid")
+		return fmt.Errorf("block validation failed: Signature is invalid")
 	}
 
 	return nil
+}
+
+func ValidateBlock(currentBlock, prevBlock block.Block) error {
+	genesisValidator := DefaultGenesisBlockValidator{}
+	blockValidator := NewDefaultBlockValidator(genesisValidator)
+	return blockValidator.Validate(currentBlock, prevBlock)
 }
