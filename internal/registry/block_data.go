97e21e190b4f68eec9568fc40ba357753942bf15
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-19 11:48:15 +0200

    refactor(block_data.go): remove unused imports and variables, simplify NewBlockData function signature, and remove AddBlockToRegistry and ConvertToBlock functions as they are no longer needed

--- internal/registry/block_data.go
+++ internal/registry/block_data.go
@@ -1,15 +1,10 @@
 package registry
 
 import (
-	"os"
-
 	"github.com/ipfs/boxo/path"
 	"github.com/ipfs/go-cid"
-	ipfsLog "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/pierreleocadie/SecuraChain/internal/config"
 	"github.com/pierreleocadie/SecuraChain/internal/core/block"
-	"github.com/pierreleocadie/SecuraChain/pkg/utils"
 )
 
 // BlockData represents the data associated with a block stored with IPFS.
@@ -20,55 +15,11 @@ type BlockData struct {
 	Provider peer.AddrInfo `json:"provider"`
 }
 
-// BlockRegistry represents a collection of block records.
-type BlockRegistry struct {
-	Blocks []BlockData `json:"blocks"`
-}
-
-func NewBlockData(b *block.Block, fileCid path.ImmutablePath, provider peer.AddrInfo) BlockData {
+func NewBlockData(b block.Block, fileCid path.ImmutablePath, provider peer.AddrInfo) BlockData {
 	return BlockData{
 		ID:       b.Header.Height,
 		Key:      block.ComputeHash(b),
 		BlockCid: fileCid.RootCid(),
 		Provider: provider,
 	}
-}
-
-// AddBlockToRegistry adds a block and the data associated to the registry.
-func AddBlockToRegistry(log *ipfsLog.ZapEventLogger, b *block.Block, config *config.Config, fileCid path.ImmutablePath, provider peer.AddrInfo) error {
-	var blockRegistery BlockRegistry
-
-	// Load existing registry if it exists
-	blockRegistery, err := LoadRegistryFile[BlockRegistry](log, config, config.BlockRegistryPath)
-	if err != nil && !os.IsNotExist(err) {
-		log.Errorln("Error loading block registry:", err)
-		return err
-	}
-
-	newData := NewBlockData(b, fileCid, provider)
-
-	log.Debugln("New BlockData : ", newData)
-
-	blockRegistery.Blocks = append(blockRegistery.Blocks, newData)
-
-	// Save updated registry back to file
-	log.Infoln("Block registry created or updated successfully")
-	return SaveRegistryToFile(log, config, blockRegistery)
-}
-
-// ConvertToBlock reads the contents of the file at the given file path and converts it into a block.Block object.
-func ConvertToBlock(log *ipfsLog.ZapEventLogger, filePath string) (*block.Block, error) {
-	filePath, err := utils.SanitizePath(filePath)
-	if err != nil {
-		log.Errorf("Error sanitizing file patxh %v\n", err)
-		return nil, err
-	}
-	data, err := os.ReadFile(filePath) //#nosec G304
-	if err != nil {
-		log.Errorf("Error reading file %v\n", err)
-		return nil, err
-	}
-
-	log.Debugln("File converted to block successfully")
-	return block.DeserializeBlock(data)
 }
