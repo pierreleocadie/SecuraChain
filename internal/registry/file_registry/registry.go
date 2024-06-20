1ab9948eacfa529a7a30b88fa4638e3c6dcfb720
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-21 00:17:08 +0200

    refactor(file_registry): import block package in file_registry to use Block type in UpdateFromBlock method

--- internal/registry/file_registry/registry.go
+++ internal/registry/file_registry/registry.go
@@ -1,9 +1,13 @@
 package fileregistry
 
-import "github.com/pierreleocadie/SecuraChain/internal/core/transaction"
+import (
+	"github.com/pierreleocadie/SecuraChain/internal/core/block"
+	"github.com/pierreleocadie/SecuraChain/internal/core/transaction"
+)
 
 type FileRegistry interface {
 	Add(addFileTransac transaction.AddFileTransaction) error
 	Get(myPublicKey string) []FileData
+	UpdateFromBlock(b block.Block) error
 	Delete(deleteFileTransac transaction.DeleteFileTransaction) error
 }
