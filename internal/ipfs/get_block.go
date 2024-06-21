a88b1dea97cf6006159d381ff61cf3897cfc6806
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-21 14:40:35 +0200

    refactor(ipfs): update GetBlock function to return block.Block instead of *block.Block for consistency and improve error handling in file operations

--- internal/ipfs/get_block.go
+++ internal/ipfs/get_block.go
@@ -10,33 +10,32 @@ import (
 	ipfsLog "github.com/ipfs/go-log/v2"
 	icore "github.com/ipfs/kubo/core/coreiface"
 	"github.com/pierreleocadie/SecuraChain/internal/core/block"
-	"github.com/pierreleocadie/SecuraChain/internal/registry"
 )
 
 // GetBlock retrieves a block from IPFS using the provided CID.
-func GetBlock(log *ipfsLog.ZapEventLogger, ctx context.Context, ipfsAPI icore.CoreAPI, blockPath path.ImmutablePath) (*block.Block, error) {
+func GetBlock(log *ipfsLog.ZapEventLogger, ctx context.Context, ipfsAPI icore.CoreAPI, blockPath path.ImmutablePath) (block.Block, error) {
 	blockFetched, err := GetFile(ctx, ipfsAPI, blockPath)
 	if err != nil {
 		log.Errorln("Failed to get the file: %s ", err)
-		return nil, fmt.Errorf("failed to get the file: %v", err)
+		return block.Block{}, fmt.Errorf("failed to get the file: %v", err)
 	}
 
 	if err := files.WriteTo(blockFetched, "block"); err != nil {
 		log.Errorln("Failed to write the block into a file: %s ", err)
-		return nil, fmt.Errorf("failed to create file for the block: %v", err)
+		return block.Block{}, fmt.Errorf("failed to create file for the block: %v", err)
 	}
 	log.Debugln("Wrote the block into a file")
 
-	b, err := registry.ConvertToBlock(log, "block")
+	b, err := ConvertBytesToBlock(log, "block")
 	if err != nil {
 		log.Errorln("Failed to convert the file into a *block.Block: %s ", err)
-		return nil, fmt.Errorf("failed to converted the file into a *block.Block: %v", err)
+		return block.Block{}, fmt.Errorf("failed to converted the file into a *block.Block: %v", err)
 	}
 	log.Debugln("Fetched CID converted to a block")
 
 	if err := os.Remove("block"); err != nil {
 		log.Errorln("Failed to remove the file 'block': %s ", err)
-		return nil, fmt.Errorf("failed to remove the file 'block': %v", err)
+		return block.Block{}, fmt.Errorf("failed to remove the file 'block': %v", err)
 	}
 	log.Debugln("file removed")
 
