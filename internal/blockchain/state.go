ecc7b4aab3ed36ba561df608cfe605fbe3c282ea
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-21 14:25:44 +0200

    feat(state.go): create State interface in blockchain package to define methods for handling blocks, syncing blockchain, and post-sync operations

---
+++ internal/blockchain/state.go
@@ -0,0 +1,9 @@
+package blockchain
+
+import "github.com/pierreleocadie/SecuraChain/internal/core/block"
+
+type State interface {
+	HandleBlock(block block.Block)
+	SyncBlockchain()
+	PostSync()
+}
