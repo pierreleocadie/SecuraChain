5323e95bbdd29a24eff6012116a320fe7a38eddc
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-23 13:11:47 +0200

    refactor(ipfs): refactor UnpinFile method to use IPFSNode struct fields for API and Ctx to improve code readability and maintainability

--- internal/ipfs/unpin_file.go
+++ internal/ipfs/unpin_file.go
@@ -1,23 +1,19 @@
 package ipfs
 
 import (
-	"context"
 	"fmt"
 	"log"
 
 	"github.com/ipfs/boxo/path"
-	icore "github.com/ipfs/kubo/core/coreiface"
 )
 
-func UnpinFile(ctx context.Context, ipfsAPI icore.CoreAPI, fileCid path.ImmutablePath) error {
-	if err := ipfsAPI.Pin().Rm(ctx, fileCid); err != nil {
-		log.Printf("Could not unpin file %v", err)
+func (ipfs *IPFSNode) UnpinFile(fileCid path.ImmutablePath) error {
+	if err := ipfs.API.Pin().Rm(ipfs.Ctx, fileCid); err != nil {
 		return fmt.Errorf("could not unpin file %v", err)
 	}
 
-	_, IsUnpinned, err := ipfsAPI.Pin().IsPinned(ctx, fileCid)
+	_, IsUnpinned, err := ipfs.API.Pin().IsPinned(ipfs.Ctx, fileCid)
 	if err != nil {
-		log.Printf("Could not check if file is unpinned %v", err)
 		return fmt.Errorf("could not check if file is unpinned %v", err)
 	}
 
