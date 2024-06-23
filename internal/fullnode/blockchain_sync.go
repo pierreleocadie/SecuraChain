b4d7725a3fe68624c8dc10a7282dafd49dcd2ff9
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-23 13:36:36 +0200

    refactor(blockchain_sync.go): refactor GetMissingBlocks function to use blockregistry.BlockRegistry and blockchaindb.BlockchainDB interfaces for better abstraction and flexibility

--- internal/fullnode/blockchain_sync.go
+++ internal/fullnode/blockchain_sync.go
@@ -3,30 +3,29 @@ package fullnode
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	"github.com/ipfs/boxo/path"
 	ipfsLog "github.com/ipfs/go-log/v2"
-	icore "github.com/ipfs/kubo/core/coreiface"
 	"github.com/pierreleocadie/SecuraChain/internal/blockchaindb"
 	"github.com/pierreleocadie/SecuraChain/internal/core/block"
 	"github.com/pierreleocadie/SecuraChain/internal/ipfs"
-	"github.com/pierreleocadie/SecuraChain/internal/registry"
+	blockregistry "github.com/pierreleocadie/SecuraChain/internal/registry/block_registry"
 )
 
 // GetMissingBlocks returns a list of missing blocks by comparing the blocks in the given BlockRegistry
 // with the blocks stored in the PebbleDB. It checks if each block exists in the database and if not,
 // adds it to the list of missing blocks.
-func GetMissingBlocks(log *ipfsLog.ZapEventLogger, r registry.BlockRegistry, db *blockchaindb.PebbleDB) []*registry.BlockData {
-	var missingBlocks []*registry.BlockData
+func GetMissingBlocks(log *ipfsLog.ZapEventLogger, r blockregistry.BlockRegistry, db blockchaindb.BlockchainDB) []blockregistry.BlockData {
+	var missingBlocks []blockregistry.BlockData
 
-	for _, blockData := range r.Blocks {
-
-		if existingBlock, err := db.GetBlock(log, blockData.Key); err == nil && existingBlock != nil {
+	for _, blockData := range r.GetBlocks() {
+		if existingBlock, err := db.GetBlock(blockData.Key); err == nil && !reflect.DeepEqual(existingBlock, block.Block{}) {
 			log.Errorln("Block already exists in the blockchain : ", blockData.Key)
 			continue
 		}
 
-		missingBlocks = append(missingBlocks, &blockData)
+		missingBlocks = append(missingBlocks, blockData)
 	}
 
 	log.Infoln("Number of missing blocks : ", len(missingBlocks))
@@ -34,20 +33,20 @@ func GetMissingBlocks(log *ipfsLog.ZapEventLogger, r registry.BlockRegistry, db 
 }
 
 // DownloadMissingBlocks attemps to download blocks that are missing in the local blockchain from IPFS.
-func DownloadMissingBlocks(log *ipfsLog.ZapEventLogger, ctx context.Context, ipfsAPI icore.CoreAPI, missingBlocks []*registry.BlockData) ([]*block.Block, error) {
-	var downloadedBlocks []*block.Block
+func DownloadMissingBlocks(log *ipfsLog.ZapEventLogger, ctx context.Context, ipfsNode ipfs.IPFSNode, missingBlocks []blockregistry.BlockData) ([]block.Block, error) {
+	var downloadedBlocks []block.Block
 
 	for _, blockData := range missingBlocks {
 		blockPath := path.FromCid(blockData.BlockCid)
 		log.Debugln("Converted block CID to pathImmutable : ", blockPath.String())
 
-		if err := ipfsAPI.Swarm().Connect(ctx, blockData.Provider); err != nil {
+		if err := ipfsNode.API.Swarm().Connect(ctx, blockData.Provider); err != nil {
 			log.Errorln("failed to connect to provider: %s", err)
 			continue
 		}
 		log.Debugln("Connected to provider %s", blockData.Provider.ID)
 
-		b, err := ipfs.GetBlock(log, ctx, ipfsAPI, blockPath)
+		b, err := ipfsNode.GetBlock(blockPath)
 		if err != nil {
 			log.Errorln("Error downloading block from IPFS : ", err)
 			return nil, fmt.Errorf("error downloading block from IPFS : %s", err)
