723b34114a41986384e78c2d7ef49dffc2acf125
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-23 20:35:29 +0200

    feat(file_request.go): add AskForMyFiles function to request files over the network and handle responses efficiently

---
+++ internal/registry/file_registry/file_request.go
@@ -0,0 +1,69 @@
+package fileregistry
+
+import (
+	"context"
+
+	ipfsLog "github.com/ipfs/go-log/v2"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+)
+
+// func publishMyFileRequest(log *ipfsLog.ZapEventLogger, ctx context.Context, askMyFiles *pubsub.Topic, myPublicKey []byte) error {
+// 	log.Debugln("Requesting for my files from the network")
+// 	if err := askMyFiles.Publish(ctx, myPublicKey); err != nil {
+// 		log.Errorln("Error publishing my files request : ", err)
+// 		return err
+// 	}
+// 	return nil
+// }
+
+// func receiveMyFileResponse(log *ipfsLog.ZapEventLogger, ctx context.Context, recMyFiles *pubsub.Subscription) {
+// 	myFiles := make(chan []byte)
+// 	senderID := make(chan string)
+
+// 	go func() {
+// 		for {
+// 			msg, err := recMyFiles.Next(ctx)
+// 			if err != nil {
+// 				log.Errorln("Error receiving message from network: ", err)
+// 				return
+// 			}
+// 			if msg != nil {
+// 				log.Debugln("My files received from : ", senderID)
+// 				log.Debugln("My files received : ", string(msg.Data))
+// 				myFiles <- msg.Data
+// 				senderID <- msg.GetFrom().String()
+// 				return
+// 			}
+// 		}
+// 	}()
+// }
+
+// AskForIndexingRegistry sends a request for the indexing registry over the network.
+func AskForMyFiles(log *ipfsLog.ZapEventLogger, ctx context.Context, askMyFiles *pubsub.Topic, recMyFiles *pubsub.Subscription, myPublicKey []byte) ([]byte, string, error) {
+	log.Debugln("Requesting for my files from the network")
+	if err := askMyFiles.Publish(ctx, myPublicKey); err != nil {
+		log.Errorln("Error publishing my files request : ", err)
+		return nil, "", err
+	}
+
+	myFiles := make(chan []byte)
+	senderID := make(chan string)
+	go func() {
+		for {
+			msg, err := recMyFiles.Next(ctx)
+			if err != nil {
+				log.Errorln("Error receiving message from network: ", err)
+				break
+			}
+			if msg != nil {
+				log.Debugln("My files received from : ", senderID)
+				log.Debugln("My files received : ", string(msg.Data))
+				myFiles <- msg.Data
+				senderID <- msg.GetFrom().String()
+				break
+			}
+		}
+	}()
+
+	return <-myFiles, <-senderID, nil
+}
