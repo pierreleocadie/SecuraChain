e09253d56f94c8a98c9b66f927bfea583ab8cf04
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-23 12:48:43 +0200

    refactor(ipfs): refactor AddFile method to be a method of IPFSNode struct for better encapsulation and readability

--- internal/ipfs/add_file.go
+++ internal/ipfs/add_file.go
@@ -2,26 +2,21 @@
 package ipfs
 
 import (
-	"context"
 	"fmt"
 	"log"
 
 	"github.com/ipfs/boxo/path"
-	icore "github.com/ipfs/kubo/core/coreiface"
-	"github.com/pierreleocadie/SecuraChain/internal/config"
 )
 
 // AddFile adds a file to IPFS and returns the CID of the file.
-func AddFile(ctx context.Context, config *config.Config, ipfsAPI icore.CoreAPI, filePath string) (path.ImmutablePath, error) {
-	file, err := PrepareFileForIPFS(filePath)
+func (ipfs *IPFSNode) AddFile(filePath string) (path.ImmutablePath, error) {
+	file, err := ipfs.prepareFileForIPFS(filePath)
 	if err != nil {
-		log.Println("could not get File:", err)
 		return path.ImmutablePath{}, fmt.Errorf("could not get File: %v", err)
 	}
 
-	fileCid, err := ipfsAPI.Unixfs().Add(ctx, file)
+	fileCid, err := ipfs.API.Unixfs().Add(ipfs.Ctx, file)
 	if err != nil {
-		log.Printf("Could not add file to IPFS %v", err)
 		return path.ImmutablePath{}, fmt.Errorf("could not add file to IPFS %v", err)
 	}
 
