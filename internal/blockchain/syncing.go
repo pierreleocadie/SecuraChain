062ad6b459216028f6fe37f55c70a5e0b7703a86
Pierre Leocadie <68948760+pierreleocadie@users.noreply.github.com>
2024-06-26 06:22:06 +0200

    refactor(syncing.go): move log statement to the correct position for better readability and clarity

--- internal/blockchain/syncing.go
+++ internal/blockchain/syncing.go
@@ -110,9 +110,10 @@ func (s *SyncingState) SyncBlockchain() {
 	// 7 . Clear the black list
 	s.blockchain.nodeBlacklist = []string{}
 
+	s.blockchain.log.Debugln("Blockchain synchronized with the network")
+
 	// 8 . Change the state of the node
 	s.blockchain.SetState(s.blockchain.PostSyncState)
-	s.blockchain.log.Debugln("Blockchain synchronized with the network")
 }
 
 func (s *SyncingState) PostSync() {
